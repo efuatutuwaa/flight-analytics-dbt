9003b544d18e19c6ad510b54e2d3034da8210063
Afua <etutuwaa.88@gmail.com>
2026-01-01 06:55:23 +0200

    Added new column 'codeshare_flight_number' to model

--- models/staging/stg_codeshare.sql
+++ models/staging/stg_codeshare.sql
@@ -8,6 +8,7 @@ cleaned as (
     select
         codeshare_id,
         flight_id,
+        flight_number as codeshare_flight_number,
         airline_id as marketing_airline_id,
         airline_name as marketing_airline_name
 
