2acc84f7ff2c36424decc0f1ca90b156c7646499
Afua <etutuwaa.88@gmail.com>
2026-01-01 11:39:35 +0200

    Add int_flight_routes with UTC-normalized timestamps and duration metrics

--- models/intermediate/int_flight_routes.sql
+++ models/intermediate/int_flight_routes.sql
@@ -1,31 +1,39 @@
-select
+
+with base as (
+    select
     -- identifiers --
     fd.flight_id,
     fd.departure_airport_id,
     fd.arrival_airport_id,
+    fd.flight_status,
+    dep_geo.airport_timezone_name as departure_airport_timezone,
+    arr_geo.airport_timezone_name as arrival_airport_timezone,
 
     -- routes geometry --
     r.route_distance_km,
 
 
-    -- scheduled times --
-    dep.scheduled_departure_time,
-    arr.scheduled_arrival_time,
-    extract(epoch from (arr.scheduled_arrival_time - dep.scheduled_departure_time )) / 60
-        as scheduled_duration_minutes,
+    -- scheduled times in local time --
+    dep.scheduled_departure_time as scheduled_departure_time_local,
+    arr.scheduled_arrival_time as scheduled_arrival_time_local,
 
-    -- actual times --
-    dep.actual_departure_time,
-    arr.actual_arrival_time,
-    extract(epoch from ( arr.actual_arrival_time - dep.actual_departure_time )) / 60
-        as actual_duration_minutes,
+    -- actual times in local time --
 
-    -- operational column --
-    fd.flight_status,
-    case
-        when fd.flight_status = 'landed' then 'no'
-        else 'yes'
-    end as is_cancelled
+    dep.actual_departure_time as actual_departure_time_local,
+    arr.actual_arrival_time as actual_arrival_time_local,
+
+    -- scheduled times in utc --
+    dep.scheduled_departure_time at time zone dep_geo.airport_timezone_name
+        as scheduled_departure_time_utc,
+    arr.scheduled_arrival_time at time zone arr_geo.airport_timezone_name
+        as scheduled_arrival_time_utc,
+
+    -- actual times in utc --
+     dep.actual_departure_time at time zone dep_geo.airport_timezone_name
+        as actual_departure_time_utc,
+    arr.actual_arrival_time at time zone arr_geo.airport_timezone_name
+        as actual_arrival_time_utc
+
 
 from {{ ref ('stg_flight_details') }} fd
 left join {{ ref ('stg_routes') }} r
@@ -33,4 +41,24 @@ left join {{ ref ('stg_routes') }} r
 left join {{ ref ('stg_departures') }} dep
     on fd.flight_id = dep.flight_id
 left join {{ ref ('stg_arrivals') }} arr
-    on fd.flight_id = arr.flight_id;
+    on fd.flight_id = arr.flight_id
+left join {{ ref ('int_airport_geography')  }} dep_geo
+    on fd.departure_airport_id = dep_geo.airport_id
+left join {{ ref ('int_airport_geography')  }} arr_geo
+    on fd.arrival_airport_id = arr_geo.airport_id
+
+)
+
+select 
+    *, 
+
+    extract(epoch from (scheduled_arrival_time_utc - scheduled_departure_time_utc)) / 60
+        as scheduled_duration_minutes,
+    extract(epoch from (actual_arrival_time_utc - actual_departure_time_utc)) / 60
+        as actual_duration_minutes,
+    case
+        when flight_status = 'landed' then 'no'
+        else 'yes'
+    end as is_cancelled
+from base;
+
