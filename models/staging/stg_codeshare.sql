e61710e72578b7608a5ddb931492e2f45e463572
Afua <etutuwaa.88@gmail.com>
2026-01-01 05:27:26 +0200

    Added 'stg_codeshare' model

---
+++ models/staging/stg_codeshare.sql
@@ -0,0 +1,16 @@
+with source as (
+    select
+        *
+    from {{ source ('flights', 'codeshare') }}
+),
+
+cleaned as (
+    select
+        codeshare_id,
+        flight_id,
+        airline_id as marketing_airline_id,
+        airline_name as marketing_airline_name
+
+    from source
+
+)
