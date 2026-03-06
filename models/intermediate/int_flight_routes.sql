417cf5c478346a5c70495b3338759302c51285ff
Efua Tutuwaa-Ampofo <efuatutuwaa-ampofo@Efuas-MacBook-Air.local>
2026-03-06 02:45:57 +0200

    feat(int_flight_routes): add delay metrics, flight_date, flight_number; fix is_cancelled logic

--- models/intermediate/int_flight_routes.sql
+++ models/intermediate/int_flight_routes.sql
@@ -6,6 +6,8 @@ with base as (
     fd.departure_airport_id,
     fd.arrival_airport_id,
     fd.flight_status,
+    fd.flight_date,
+    fd.flight_number,
     dep_geo.airport_timezone_name as departure_airport_timezone,
     arr_geo.airport_timezone_name as arrival_airport_timezone,
 
@@ -23,42 +25,42 @@ with base as (
     arr.actual_arrival_time as actual_arrival_time_local,
 
     -- scheduled times in utc --
-to_utc_timestamp(
-    dep.scheduled_departure_time,
-    case
-        when dep_geo.airport_timezone_name in ('Unknown', '', null)
-            then 'UTC'
-        else dep_geo.airport_timezone_name
-    end
-) as scheduled_departure_time_utc,
+    to_utc_timestamp(
+        dep.scheduled_departure_time,
+        case
+            when dep_geo.airport_timezone_name in ('Unknown', '', null)
+                then 'UTC'
+            else dep_geo.airport_timezone_name
+        end
+    ) as scheduled_departure_time_utc,
 
-to_utc_timestamp(
-    arr.scheduled_arrival_time,
-    case
-        when arr_geo.airport_timezone_name in ('Unknown', '', null)
-            then 'UTC'
-        else arr_geo.airport_timezone_name
-    end
-) as scheduled_arrival_time_utc,
+    to_utc_timestamp(
+        arr.scheduled_arrival_time,
+        case
+            when arr_geo.airport_timezone_name in ('Unknown', '', null)
+                then 'UTC'
+            else arr_geo.airport_timezone_name
+        end
+    ) as scheduled_arrival_time_utc,
 
--- actual times in utc --
-to_utc_timestamp(
-    dep.actual_departure_time,
-    case
-        when dep_geo.airport_timezone_name in ('Unknown', '', null)
-            then 'UTC'
-        else dep_geo.airport_timezone_name
-    end
-) as actual_departure_time_utc,
+    -- actual times in utc --
+    to_utc_timestamp(
+        dep.actual_departure_time,
+        case
+            when dep_geo.airport_timezone_name in ('Unknown', '', null)
+                then 'UTC'
+            else dep_geo.airport_timezone_name
+        end
+    ) as actual_departure_time_utc,
 
-to_utc_timestamp(
-    arr.actual_arrival_time,
-    case
-        when arr_geo.airport_timezone_name in ('Unknown', '', null)
-            then 'UTC'
-        else arr_geo.airport_timezone_name
-    end
-) as actual_arrival_time_utc
+    to_utc_timestamp(
+        arr.actual_arrival_time,
+        case
+            when arr_geo.airport_timezone_name in ('Unknown', '', null)
+                then 'UTC'
+            else arr_geo.airport_timezone_name
+        end
+    ) as actual_arrival_time_utc
 
 from {{ ref ('stg_flight_details') }} fd
 left join {{ ref ('stg_routes') }} r
@@ -82,14 +84,29 @@ select
         scheduled_departure_time_utc,
         scheduled_arrival_time_utc
     ) as scheduled_duration_minutes,
+
+
     timestampdiff(
         MINUTE,
         actual_departure_time_utc,
         actual_arrival_time_utc
     ) as actual_duration_minutes,
+
+    timestampdiff(
+        MINUTE, 
+        scheduled_departure_time_utc,
+        actual_departure_time_utc
+    ) as departure_delay_minutes,
+
+    timestampdiff(
+        MINUTE, 
+        scheduled_arrival_time_utc,
+        actual_arrival_time_utc
+    ) as arrival_delay_minutes,
+
     case
-        when flight_status = 'landed' then 'no'
-        else 'yes'
+        when flight_status = 'cancelled' then true
+        else false
     end as is_cancelled
 from base;
 
